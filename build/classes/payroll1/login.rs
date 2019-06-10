a0fd3b669c5c958dcd5ab2ccda9c83dc78a07694
manugowda287 <51666239+manugowda287@users.noreply.github.com>
2019-06-10 21:40:52 +0530

    Add files via upload

---
+++ build/classes/payroll1/login.rs
@@ -0,0 +1,8 @@
+employee.payroll.system.login$2
+employee.payroll.system.login$1
+employee.payroll.system.login$4
+employee.payroll.system.login$3
+employee.payroll.system.login$6
+employee.payroll.system.login$5
+employee.payroll.system.login$7
+employee.payroll.system.login
